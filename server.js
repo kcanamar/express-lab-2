2fe11e6934520b052fa6d1f89c2ae10052e43851
Kyle Canamar <canamar.kyle@gmail.com>
2022-04-02 17:36:14 -0700

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.get('/greeting', function (req,res) {
 app.get('/greeting/:name', function (req,res) {
     res.send(`Hello, ${req.params.name}`);
 })
-
+app.get('/tip/:total/:tipPercentage', function (req,res) {
+    res.send(`How much you should tip is ${(parseInt(req.params.total) * parseInt(req.params.tipPercentage) / parseInt(req.params.total))}`);
+})
 ///////////////////////////
 // Server Listener
 ///////////////////////////
