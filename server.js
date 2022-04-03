6adb2c7454e017862b0018e7f23ed907a5a7faa9
Kyle Canamar <canamar.kyle@gmail.com>
2022-04-02 19:54:32 -0700

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.get('/greeting/:name', function (req,res) {
 app.get('/tip/:total/:tipPercentage', function (req,res) {
     res.send(`How much you should tip is ${(parseInt(req.params.total) * parseInt(req.params.tipPercentage) / parseInt(req.params.total))}`);
 })
+app.get('/magic/:phrase', function (req,res) {
+    const ball = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+    let rand = Math.floor(Math.random()*20)
+    res.send(`<h1>${req.params.phrase}</h1> </br> <h1>${ball[rand]}</h1>`);
+})
 ///////////////////////////
 // Server Listener
 ///////////////////////////
